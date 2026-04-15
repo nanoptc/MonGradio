09c025c7b8065e992e45211dc64ebadb102def4b
NanoPTC <nanoptc@gmail.com>
2026-04-15 09:43:17 +0100

    Script Gradio
    Il est opérationnel

---
+++ app.py
@@ -0,0 +1,9 @@
+import gradio as gr
+
+def salutation(nom):
+    return f"Félicitations {nom}, votre premier environnement GitHub et Gradio fonctionne !"
+
+demo = gr.Interface(fn=salutation, inputs="text", outputs="text")
+
+if __name__ == "__main__":
+    demo.launch()
